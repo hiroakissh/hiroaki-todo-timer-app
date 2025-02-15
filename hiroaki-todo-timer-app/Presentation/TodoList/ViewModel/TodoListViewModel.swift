574f7b0ed52b1f0f9594195a74c3fb1f7ac0c0cb
hiroakissh <hiroaki.saito1014@gmail.com>
2025-02-15 17:51:19 +0900

    feat: ViewModelファイルの作成

--- hiroaki-todo-timer-app/Presentation/TodoList/ViewModel/TodoListViewModel.swift
+++ hiroaki-todo-timer-app/Presentation/TodoList/ViewModel/TodoListViewModel.swift
@@ -1,7 +1,44 @@
 //
-//  Untitled.swift
+//  TodoListViewModel.swift
 //  hiroaki-todo-timer-app
 //
 //  Created by HiroakiSaito on 2025/02/15.
 //
 
+import SwiftData
+
+@Observable
+@MainActor
+final class TodoViewModel {
+
+    private let useCase: TodoUseCase
+    var todos: [TodoModel] = []
+
+    init(useCase: TodoUseCase) {
+        self.useCase = useCase
+        fetchAllTodos()
+    }
+
+    /// **タスクを取得して ViewModel にセット**
+    func fetchAllTodos() {
+        todos = useCase.fetchAllTodos()
+    }
+
+    /// **タスクを追加**
+    func addTodo(title: String, priority: TaskPriority) {
+        useCase.createTodo(title: title, priority: priority)
+        fetchAllTodos()
+    }
+
+    /// **タスクを更新**
+    func updateTodo(_ todo: TodoModel) {
+        useCase.updateTodo(todo)
+        fetchAllTodos()
+    }
+
+    /// **タスクを削除**
+    func deleteTodo(_ todo: TodoModel) {
+        useCase.deleteTodo(todo)
+        fetchAllTodos()
+    }
+}
